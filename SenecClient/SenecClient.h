cc341781abb33042ce343828515ff181def76a59
André Rohrbeck <andre.rohrbeck@prose.one>
2018-10-06 22:35:59 +0200

    Initial commit

---
+++ SenecClient/SenecClient.h
@@ -0,0 +1,18 @@
+//
+//  SenecClient.h
+//  SenecClient
+//
+//  Created by André Rohrbeck on 06.10.18.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SenecClient.
+FOUNDATION_EXPORT double SenecClientVersionNumber;
+
+//! Project version string for SenecClient.
+FOUNDATION_EXPORT const unsigned char SenecClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SenecClient/PublicHeader.h>
+
+
