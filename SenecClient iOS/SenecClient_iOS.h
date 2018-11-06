be006edcf585ce2282dd0cfb0fdbad710bdce7b3
André Rohrbeck <andre.rohrbeck@prose.one>
2018-11-06 23:28:59 +0100

    Removed all UI stuff from the SenecClient project (moved to SenecUI project)

---
+++ SenecClient iOS/SenecClient_iOS.h
@@ -0,0 +1,18 @@
+//
+//  SenecClient_iOS.h
+//  SenecClient iOS
+//
+//  Created by André Rohrbeck on 22.10.18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SenecClient_iOS.
+FOUNDATION_EXPORT double SenecClient_iOSVersionNumber;
+
+//! Project version string for SenecClient_iOS.
+FOUNDATION_EXPORT const unsigned char SenecClient_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SenecClient_iOS/PublicHeader.h>
+
+
---
+++ SenecClient iOSTests/SenecClient_iOSTests.swift
@@ -0,0 +1,13 @@
+//
+//  SenecClient_iOSTests.swift
+//  SenecClient iOSTests
+//
+//  Created by André Rohrbeck on 22.10.18.
+//
+
+import XCTest
+@testable import SenecClient_iOS
+
+class SenecClient_iOSTests: XCTestCase {
+    
+}
